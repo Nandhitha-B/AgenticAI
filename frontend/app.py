f1dca327e5703df0c312ee7d1c0b101e9bd52166
Nandhitha <bnandhitha0609@gmail.com>
2026-04-08 17:27:36 +0530

    Changed frontend for port

--- frontend/app.py
+++ frontend/app.py
@@ -254,4 +254,9 @@ with gr.Blocks(theme=gr.themes.Soft()) as demo:
         )
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860)
+    port = int(os.environ.get("PORT", 7860))
+
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=port
+    )
